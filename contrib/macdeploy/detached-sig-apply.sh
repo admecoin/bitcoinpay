50b55c32a6bfbb8b2967afe30d0e44c19b8b2b42
mammix2 <mammix2@hotmail.com>
2019-06-19 23:01:29 +0100

    contrib: various fixes, case and naming

--- contrib/macdeploy/detached-sig-apply.sh
+++ contrib/macdeploy/detached-sig-apply.sh
@@ -5,7 +5,7 @@ UNSIGNED=$1
 SIGNATURE=$2
 ARCH=x86_64
 ROOTDIR=dist
-BUNDLE=${ROOTDIR}/BitcoinPay-Qt.app
+BUNDLE=${ROOTDIR}/bitcoinpay-Qt.app
 TEMPDIR=signed.temp
 OUTDIR=signed-app
 
--- contrib/macdeploy/detached-sig-create.sh
+++ contrib/macdeploy/detached-sig-create.sh
@@ -2,7 +2,7 @@
 set -e
 
 ROOTDIR=dist
-BUNDLE=${ROOTDIR}/BitcoinPay-Qt.app
+BUNDLE=${ROOTDIR}/bitcoinpay-Qt.app
 CODESIGN=codesign
 TEMPDIR=sign.temp
 TEMPLIST=${TEMPDIR}/signatures.txt
