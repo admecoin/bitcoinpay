b3d2e65c643035a449da6709e152ef6213e41399
mammix2 <mammix2@hotmail.com>
2019-07-16 20:46:57 +0100

    fix URLs

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -39,7 +39,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/mammix2/bitcoinpay
+-u|--url	Specify the URL of the repository. Default is https://github.com/bitcoinpay-project/bitcoin-pay
 -v|--verify 	Verify the gitian build
 -b|--build	Do a gitian build
 -s|--sign	Make signed binaries for Windows and Mac OSX
@@ -232,8 +232,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/mammix2/bitcoinpay/gitian.sigs.git
-    git clone https://github.com/mammix2/bitcoinpay/bitcoinpay-detached-sigs.git
+    git clone https://github.com/bitcoinpay-project/bitcoin-pay/gitian.sigs.git
+    git clone https://github.com/bitcoinpay-project/bitcoin-pay/bitcoinpay-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
